7520e4c7721a4155caf3da4d5f75d1cef5e28570
Hayk <haykm2006@gmail.com>
2021-03-09 18:08:04 +0300

    GET_request_writing

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const fs = require('fs');
 const hash = require('object-hash');
 
 const PORT = 3000;
+
+http.createServer((req, res) => {
+
+    if (req.method == 'GET') {
+        const pageTxt = fs.readFileSync('./public/index.html', 'utf-8');
+        res.setHeader('Content-type', 'text/html');
+        res.end(pageTxt);
+
+    }
+
+}).listen(PORT, () => {
+    console.log(`server has been start in port ${PORT}`);
+});
